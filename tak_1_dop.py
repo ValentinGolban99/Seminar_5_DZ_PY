700585f913fe14bb5459a9cc58a0a5ff27c06663
Valentin <111140062+ValentinGolban99@users.noreply.github.com>
2022-10-25 22:16:44 +0300

    попытка через функцию(пока не очень удачно)

--- tak_1_dop.py
+++ tak_1_dop.py
@@ -1,32 +1,25 @@
 
-def move_us_1(candies, move_counter = 0):
-    if candies != 0:
-        us_1 = int(input(f"{user_1} возьмите конфеты(не более 28): "))
-        if us_1 <= 28:
-            move_counter += 1
-            candies -= us_1
-            print(f"Осталось {candies} конфет.")
-            if candies == 0:
-                print(f"Победил: {user_1}")
+def move_us(user, candies):
+    us = int(input(f"{user} возьмите конфеты(не более 28): "))
+    if us <= 28:
+        candies -= us
+        print(f"Осталось {candies} конфет.")
+    if candies <= 0:
+        print(f"Победил: {user}")
+    else:
+        return candies
 
-
-def move_us_2(candies, move_counter = 0):
-    if candies != 0:
-        us_1 = int(input(f"{user_1} возьмите конфеты(не более 28): "))
-        if us_1 <= 28:
-            move_counter += 1
-            candies -= us_1
-            print(f"Осталось {candies} конфет.")
-            if candies == 0:
-                print(f"Победил: {user_1}")
+    
 
 user_1 = str(input('Игрок 1: '))
 user_2 = str(input('Игрок 2: '))
-candies = 2021
-move_counter = 0
+candies = 150
 
+while candies >= 0:
+    candies_one = move_us(user_1, candies)
+    candies = move_us(user_2, candies_one)
 
-
-print(f"Игра закончена за {move_counter} хода(ов).")
+# Тут я как то недомудрил и сам себя успел несколько раз позапутывать.
+# Не совсем правильно работающий код. Решил пока оставить.
 
 
